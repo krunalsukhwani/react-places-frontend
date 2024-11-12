37fcfe616afe0b21e0179ac831919e01e0a977cf
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-11-11 19:01:47 -0500

    Users Components

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import "./UsersList.css";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 const UsersList = (props) => {
   //if USERS is empty
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No Users Found!</h2>
+        <Card>
+          <h2>No Users Found!</h2>
+        </Card>
       </div>
     );
   }
