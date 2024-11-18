d1baff322e366db0a8b5b5f7447c36ddf463b8cb
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-11-18 17:56:59 -0500

    Basic Validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -2,12 +2,24 @@ import React from "react";
 
 import "./NewPlace.css";
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validator";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" id="title" type="text" label="Title" placeholder="Enter Title"/>
-      <Input element="textarea" id="description" label="Description" rows="5"/>
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE]}
+        errorText="Please enter a valid title."
+      />
+      <Input
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description(at least 5 characters)."
+      />
     </form>
   );
 };
